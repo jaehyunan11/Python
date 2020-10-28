3de86869304c1e3ad6c0276a5433e6b6c48cc31d
jaehyun An <jaehyuna11@gmail.com>
2020-10-27 21:13:46 -0700

    add functions_basic_II

--- _python/python_fundamentals/An_Jaehyun_Functions_Basic_II/functions_basic_II.py
+++ _python/python_fundamentals/An_Jaehyun_Functions_Basic_II/functions_basic_II.py
@@ -1,25 +1,23 @@
 #1 Countdown - Create a function that accepts a number as an input. Return a new list that counts down by one, from the number (as the 0th element) down to 0 (as the last element).
 #  Example: countdown(5) should return [5,4,3,2,1,0]
 
-def countDown(num):
+def count_down(num):
     final_list = []
     for i in range(num, -1, -1):
         final_list.append(i)
     return final_list
 
-x = countDown(10)
-print(x)
+print(count_down(5))
 
 #2 Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
 #  Example: print_and_return([1,2]) should print 1 and return 2
 
-# def print_and_return(arr):
-#     for i in range (0, len.arr):
-#         print (i)
+def print_and_return(arr):
+    for i in range (0, len(arr)):
+        print (arr[i])
+        return (arr[len(arr) - 1])
         
-
-# print_and_return([1,2])
-
+print(print_and_return([1,2]))
 
 #3 First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
 #  Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
@@ -36,6 +34,36 @@ print(first_plus_length([1,2,3,4,5]))
 #4 Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
 #  Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
 #  Example: values_greater_than_second([3]) should return False
+
+def values_greater_than_second(arr):
+    new_arr = []
+    first_min = arr[0]
+    second_min = arr[0]
+    if len(arr) < 2:
+        return False
+    else:
+        for num in range(len(arr)):
+            if arr[num] < first_min:
+                first_min = arr[num]
+        for num in range(len(arr)):
+            if arr[num] != first_min and arr[num] < second_min:
+                second_min = arr[num]
+        for num in range(len(arr)):
+            if arr[num] != first_min and arr[num] != second_min:
+                new_arr.append(arr[num])
+
+    return new_arr
+
+print(values_greater_than_second([5,2,3,2,1,4]))
+
 #5 This Length, That Value - Write a function that accepts two integers as parameters: size and value. The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
 #  Example: length_and_value(4,7) should return [7,7,7,7]
 #  Example: length_and_value(6,2) should return [2,2,2,2,2,2]
+
+def length_and_value(size, value):
+    new_arr = []
+    for num in range(size):
+        new_arr.append(value)
+    return new_arr
+
+print(length_and_value(6,2))
