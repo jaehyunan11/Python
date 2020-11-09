9c5030d998fb3ec8747c099660a9bc18e5d65a47
jaehyun An <jaehyuna11@gmail.com>
2020-11-08 17:18:55 -0800

    remove django

--- django/django_intro/first_project/app_one/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def first_view(request):
-    return HttpResponse("This method is the first view in app_one")
-
-def second_view(request):
-    return HttpResponse("This method is the second view in app_one")
