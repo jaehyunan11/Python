4cb6b124c2b5e96d45faf2bfb0ec1cfb94fe906c
jaehyun An <jaehyuna11@gmail.com>
2020-11-22 20:54:09 -0800

    bubble sort

---
+++ sorting_algorithm/bubble_sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(nums):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i+1]:
+                # swap the elements
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+                # setthe flag to True to begin the new loop
+                swapped = True
+
+# Verify it works
+random_list_of_nums = [5,2,1,8,4]
+bubble_sort(random_list_of_nums)
+print(random_list_of_nums)
