d799f6477542620cac602272b4119614529810ac
jaehyun An <jaehyuna11@gmail.com>
2020-11-25 23:50:50 -0800

    selection sort

--- sorting_algorithm/selection_sort.py
+++ sorting_algorithm/selection_sort.py
@@ -1,10 +1,14 @@
 def selection_sort(list):
     for i in range(0, len(list)-1):
         smallest = i
+        print(f"smallest for i: {smallest}")
         for j in range(i+1, len(list)):
             if list[j] < list[smallest]:
                 smallest = j
+                print(f"smallest for j: {smallest}")
         list[i], list[smallest] = list[smallest], list[i]
 
     return list
 
+list = [29, 72, 98, 13, 87, 66, 52, 51, 36]
+print(selection_sort(list))
